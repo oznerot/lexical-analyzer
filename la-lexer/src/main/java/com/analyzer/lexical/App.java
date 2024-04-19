cc8f204e55720897518734a227bb5760d3377268
Vitor Enzo <vitorenzocs@gmail.com>
2024-04-19 10:26:17 -0300

    Maven Project Built

---
+++ la-lexer/src/main/java/com/analyzer/lexical/App.java
@@ -0,0 +1,13 @@
+package com.analyzer.lexical;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
